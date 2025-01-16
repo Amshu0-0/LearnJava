2b03799ee0f5e26e42efed0b244aac88f3fd04b6
Amshu0-0 <amshuw05@gmail.com>
2025-01-16 08:11:05 -0600

    Learned Basics

---
+++ basics.java
@@ -0,0 +1,193 @@
+// output
+// public class basics {
+//     public static void main(String args[]) {
+        // System.out.print("Hello World\nHello World\nhello\nworld\n");
+        // System.out.print("* * * *\n* * *\n* *\n*\n");
+        // System.out.println("* * * *");
+        // System.out.println("* * *");
+        // System.out.println("* *");
+        // System.out.println("*");
+        // int a = 10;
+        // int b = 5;
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(a + b);
+
+        // a = b;
+        // System.out.print(a);
+        // byte byt = 8;
+        // System.out.println(byt);
+        // int a = 10;
+        // int b = 5;
+        // int sum = a + b;
+        // System.out.println(sum);
+// 
+    // }
+// }
+
+// input
+// import java.util.*;
+// public class basics {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+        // for word
+        // String name = sc.next();
+        // System.out.println(name);
+
+        // for paragraphs
+        // String fullName = sc.nextLine();
+        // System.out.println(fullName);
+
+        // for numbers
+        // int num = sc.nextInt();
+        // System.out.println(num + num);
+
+        // for floats
+        // float flt = sc.nextFloat();
+        // System.out.println(flt);
+
+        // for Boolean
+        // boolean bool = sc.nextBoolean();
+        // System.out.println(bool);
+
+        // for short
+        // short shrt = sc.nextShort();
+        // System.out.println(shrt);
+
+        // for Long
+        // long lng = sc.nextLong();
+        // System.out.print(lng);
+//     }
+// }
+
+ 
+// we first ttake a as input then b as input then return a + b to the user
+// import java.util.*;
+// public class basics {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner (System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = a + b;
+//         System.out.println(sum);
+//     }
+// }
+
+// import java.util.*;
+// public class basics {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner (System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         System.out.println(a*b);
+//     }
+// }
+
+// import java.util.*;
+// public class basics {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         float radius = sc.nextFloat();
+//         float area = 3.14f * radius * radius;
+//         System.out.println(area);
+//     }
+// } 
+
+// import java.util.*;
+// public class basics {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner (System.in);
+//         float a = 25.12f;
+//         System.out.println(a);
+//         int b = (int)a;
+//         System.out.println(b);
+//     }
+// }
+
+
+// public class basics {
+//     public static void main (String args[]) {
+//         char ch = '@';
+//         int a = ch;
+//         System.out.println(a);
+//     }
+// }
+
+// public class basics {
+//     public static void main (String args[]) {
+//         char ah = 'a';
+//         System.out.println(ah);
+//         char bh = 'b';
+//         System.out.println(bh);
+//         System.out.println(ah - bh);
+//     }
+// }
+
+
+// public class basics {
+//     public static void main (String args[]) {
+//         int a = 30;
+//         long b = 31;
+//         float c = 29.99f;
+//         double d = 31.99;
+//         double e = a + b + c + d;
+//         System.out.println(e);
+//     }
+// }
+
+// import java.util.*;
+// public class basics{
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner (System.in);
+//         int A = sc.nextInt();
+//         int B = sc.nextInt();
+//         int C = sc.nextInt();
+//         int avg = ( A + B + C) / 3;
+//         System.out.println(avg);
+//     }
+// }
+
+// import java.util.*;
+// public class basics {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner (System.in);
+//         int side = sc.nextInt();
+//         int area = side * side;
+//         System.out.println(area);
+//     }
+// }
+
+// import java.util.*;
+// public class basics {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         float cost1 = sc.nextFloat();
+//         float cost2 = sc.nextFloat();
+//         float cost3 = sc.nextFloat();
+//         float cost = cost1 + cost2 + cost3;
+//         System.out.println(cost);
+//         float total = cost + (0.18f * cost);
+//         System.out.println(total);
+//     }
+// }
+
+// public class basics {
+//     public static void main(String args[]) {
+//         byte b = 4;
+//         char c = 'a';
+//         short s = 512;
+//         int i = 1000;
+//         float f = 3.14f;
+//         double d = 99.9954;
+
+//         double result = (f * b) + (i % c) - (d * s);
+//         System.out.println(result);
+//     }
+// }
+
+// public class basics {
+//     public static void main (String args[]) {
+//         int $ = 24;
+//         System.out.println($);
+//     }
+// }
---
+++ opt.java
@@ -0,0 +1,7 @@
+public class opt {
+    public static void main (String args[]) {
+        int a  = 3;
+        int b = 4;
+        int sum = a + b;
+    }
+}
