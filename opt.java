9597f8d689164222dc632cef34fcee4e3da35166
Amshu0-0 <amshuw05@gmail.com>
2025-01-16 19:40:03 -0600

    Operators Learned

--- opt.java
+++ opt.java
@@ -1,7 +1,205 @@
-public class opt {
-    public static void main (String args[]) {
-        int a  = 3;
-        int b = 4;
-        int sum = a + b;
-    }
-}
+// public class opt {
+//     public static void main (String args[]) {
+//         int a = 10;
+//         System.out.println("a " + a);
+//         int b = 5;
+//         System.out.println("b " + b);
+//         int sum = a + b;
+//         System.out.println("Sum " + sum);
+//         int diff = a - b;
+//         System.out.println("diff " + diff);
+//         int divide = a / b;
+//         System.out.println("divide " + divide);
+//         int multiply = a * b;
+//         System.out.println("multiply " + multiply);
+//         int modulo = a % b;
+//         System.out.println("modulo " + modulo);
+//     }
+// }
+
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         int a = 10;
+//         System.out.println("before writing ++a: "+a);
+//         int b = ++a;
+//         System.out.println("after writing ++a: " + a);
+//         System.out.println("variable whos value is ++a: " + b);
+//     }
+// }
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         int a = 10;
+//         System.out.println("before writing a++ a:" + a);
+//         int b = a++;
+//         System.out.println("after writing a++ a:" + a);
+//         System.out.println("b value: " + b);
+//     }
+// }
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         int a = 10;
+//         int b = --a;
+//         System.out.println("value of a is " + a);
+//         System.out.println("value of b is " + b);
+//     }
+// }
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         int a = 10;
+//         int b = a--;
+//         System.out.println("value of a is 9? " + a);
+//         System.out.println("value of b is 10? " + b);
+//     }
+// }
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         int a = 10;
+//         int b = 10;
+//         System.out.println(a == b);
+//         //returns true
+//     }
+// }
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         int a = 10;
+//         int b = 10;
+//         System.out.println(a != b);
+//         //returns false
+//     }
+// }
+
+// import java.util.*;
+// public class opt{
+//     public static void main (String args[]) {
+//         int a = 10;
+//         int b = 5;
+//         System.out.println(a > b);
+//         //returns true
+//     }
+// }
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         int a = 10;
+//         int b = 5;
+//         System.out.println(a < b);
+//         // returns false
+//     }
+// }
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         int a = 10;
+//         int b = 10;
+//         System.out.println(a >= b);
+//         // returns true
+//         a = 10;
+//         b = 5;
+//         System.out.println(a >= b);
+//         // returns true
+//     }
+// }
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         int a = 10;
+//         int b = 10;
+//         System.out.println(a <= b);
+//         // returns true
+//         a = 10;
+//         b = 5;
+//         System.out.println(a <= b);
+//         // returns false
+//     }
+// }
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         System.out.println( (3 > 2) && (2 > 1)); //return true
+//         System.out.println( (0 > 3) && (2 > 1)); //return false
+//         System.out.println( (3 > 2) && (0 > 1)); //return false
+//         System.out.println( (2 > 3) && (0 > 1)); //return false
+//     }
+// }
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         System.out.println( (3 > 2) || (2 > 1)); //return true
+//         System.out.println( (0 > 2) || (2 > 1)); //return true 
+//         System.out.println( (3 > 2) || (0 > 1)); //return true
+//         System.out.println( (0 > 2) || (0 > 1)); //return false
+//     }
+// }
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         System.out.println(!(3>2)); //return false
+//         System.out.println(!(0>2)); //return true
+//     }
+// }
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         int a = 10;
+//         a -= 5;
+//         System.out.println(a); //print 5
+//     }
+// }
+
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         int a = 10;
+//         a += a;
+//         System.out.println(a); //print 20
+//     }
+// }
+
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         int a = 10;
+//         a *= a;
+//         System.out.println(a); //print 100
+//     }
+// }
+
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         int a = 10;
+//         a /= 5;
+//         System.out.println(a); //print 2
+//     }
+// }
+
+// import java.util.*;
+// public class opt {
+//     public static void main (String args[]) {
+//         int a = 10;
+//         a %= 9;
+//         System.out.println(a); //remainder print 1
+//     }
+// }
