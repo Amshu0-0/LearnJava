5b0a08afc9d7cd25f876c1edb384919eaf51146c
Amshu0-0 <amshuw05@gmail.com>
2025-01-17 19:39:14 -0600

    Learned Basic Patterns

---
+++ patterns.java
@@ -0,0 +1,92 @@
+
+// import java.util.*;
+// public class patterns {
+//     public static void main(String[] args) {
+//         for (int index = 1; index <=4; index++) {
+//             System.out.println("*".repeat(index));
+//         }
+//     }
+// }
+
+// import java.util.*;
+// public class patterns {
+//     public static void main (String[] args) {
+//         for (int index = 1; index <= 5; index++) {
+//             //one line
+//             for (int jndex = 1; jndex <= index; jndex++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// import java.util.*;
+// public class patterns {
+//     public static void main (String[] args) {
+//         int num = 106;
+//         for (int line = 1; line <= 10; line++) {
+//             for (int star = 1; star <= num; star++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//             num -= 1;
+//         }
+//     }
+// }
+
+
+// import java.util.*;
+// public class patterns {
+//     public static void main (String[] args) {
+//         int num = 10;
+//         for (int line = 1; line <= num; line++) {
+//             for (int star = 1; star <= (num - line + 1 ); star ++ ) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// import java.util.*;
+// public class patterns {
+//     public static void main (String[] args) {
+//         for (int line = 1; line <= 4; line++) {
+//             for (int num = 1; num <=line; num++) {
+//                 System.out.print(num);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// import java.util.*;
+// public class patterns {
+//     public static void main (String[] args) {
+//         int num = 4;
+//         for (int line = 1; line <= num; line++) {
+//             for (int digit = 1; digit <= (num - line + 1); digit++) {
+//                 System.out.print(digit);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// import java.util.*;
+// public class patterns {
+//     public static void main (String[] args) {
+//         int num = 4;
+//         char ch = 'A';
+
+//         for (int line = 1; line <= num; line ++) {
+//             for (int letters = 1; letters <= line; letters++) {
+//                 System.out.print(ch);
+//                 ch++;
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
